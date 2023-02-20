40e8af9c1a05d7929a746cf3c4ccaa3130ce9890
Adrien MARION <adrien.marion@epitech.eu>
2023-02-20 12:59:47 +0100

    feat: Improve UI design for Bottom Tab Bar

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { AuthProvider } from "./contexts/AuthContext";
-import { ApplicationProvider } from "@ui-kitten/components";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import * as eva from "@eva-design/eva";
 import { default as theme } from "./adrimarn-theme.json";
 import { Router } from "./routes/Router";
 import Toast from "react-native-toast-message";
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 export default function App() {
   return (
-    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
-      <AuthProvider>
-        <Router />
-        <StatusBar style="auto" />
-        <Toast />
-      </AuthProvider>
-    </ApplicationProvider>
+      <>
+        <IconRegistry icons={EvaIconsPack}/>
+        <ApplicationProvider {...eva} theme={{...eva.dark, ...theme}}>
+        <AuthProvider>
+          <Router/>
+          <StatusBar style="auto"/>
+          <Toast/>
+        </AuthProvider>
+      </ApplicationProvider>
+      </>
+
   );
 }
--- routes/AppStack.tsx
+++ routes/AppStack.tsx
@@ -1,15 +1,51 @@
 import React from "react";
 import { HomeScreen } from "../screens/HomeScreen";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  BottomTabBarProps,
+  createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
 import { Feed } from "../screens/Feed";
 import Profile from "../screens/Profile";
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  Icon,
+} from "@ui-kitten/components";
+import { RenderProp } from "@ui-kitten/components/devsupport";
+import { ImageProps } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
+const HomeIcon: RenderProp<Partial<ImageProps>> = (props) => (
+  <Icon {...props} name="home-outline" />
+);
+
+const FeedIcon: RenderProp<Partial<ImageProps>> = (props) => (
+  <Icon {...props} name="compass-outline" />
+);
+
+const UserIcon: RenderProp<Partial<ImageProps>> = (props) => (
+  <Icon {...props} name="person-outline" />
+);
+
 export const AppStack = () => (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Feed" component={Feed} />
-      <Tab.Screen name="Profile" component={Profile} />
-    </Tab.Navigator>
-  );
+  <Tab.Navigator
+    tabBar={(props) => <BottomTabBar {...props} />}
+    screenOptions={{ headerShown: false }}
+  >
+    <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Screen name="Feed" component={Feed} />
+    <Tab.Screen name="Profile" component={Profile} />
+  </Tab.Navigator>
+);
+
+const BottomTabBar: React.FC<BottomTabBarProps> = ({ navigation, state }) => (
+  <BottomNavigation
+    selectedIndex={state.index}
+    onSelect={(index) => navigation.navigate(state.routeNames[index])}
+  >
+    <BottomNavigationTab icon={HomeIcon} />
+    <BottomNavigationTab icon={FeedIcon} />
+    <BottomNavigationTab icon={UserIcon} />
+  </BottomNavigation>
+);
